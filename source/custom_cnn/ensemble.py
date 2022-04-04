d413104b30ea7d2e8f0c41d719fc7f6626090d41
Paweł Golik <01142171@pw.edu.pl>
2022-04-04 22:37:06 +0200

    ensemble_fix

--- source/custom_cnn/ensemble.py
+++ source/custom_cnn/ensemble.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import torch
 from source.custom_cnn.image_classification_base import ImageClassificationBase
 
@@ -6,15 +7,16 @@ class MyEnsemble(ImageClassificationBase):
 
     def __init__(self, models, outputs_number):
         super(MyEnsemble, self).__init__()
-        self.models = models
+        self.models = nn.Sequential(
+            *models
+        )
         self.outputs_number = outputs_number
 
 
     def forward(self, x):
-        
         out_total = torch.zeros(x.shape[0], self.outputs_number)
         for model in self.models:
-            out = model(x.clone())  # clone to make sure x is not changed by inplace methods
+            out = model(x)  # clone to make sure x is not changed by inplace methods
             out_total += out
-       
-        return torch.softmax(out_total, dim=1)
+
+        return F.log_softmax(out_total, dim=1)
--- source/utils/reading_model.py
+++ source/utils/reading_model.py
@@ -29,14 +29,21 @@ def get_cached_net_opt_crit(params_json_path):
 
     optimizer_data['params'] = net.parameters()
     optimizer = get_object(optimizer_data)
-
+    
     criterion = get_object(criterion_data)
 
     state_file_name = data['tuning_id']
 
-    model_state, optimizer_state, criterion_state = torch.load(os.path.join(params_json_path, state_file_name))
-    net.load_state_dict(model_state)
-    optimizer.load_state_dict(optimizer_state)
-    criterion.load_state_dict(criterion_state)
+
+    try:
+        model_state, optimizer_state, criterion_state = torch.load(os.path.join(params_json_path, state_file_name))
+        net.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
+        criterion.load_state_dict(criterion_state)
+    except ValueError:
+        print(f'WARNING: No optimizer state found in the file: {params_json_path}')
+        model_state, optimizer_state = torch.load(os.path.join(params_json_path, state_file_name))
+        net.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
 
     return net, optimizer, criterion
