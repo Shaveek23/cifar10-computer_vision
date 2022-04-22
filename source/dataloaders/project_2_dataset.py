b74753f94931ea84ff005406fe25459a3e9a50ae
Paweł Golik <01142171@pw.edu.pl>
2022-04-22 22:03:05 +0200

    adding fixes to dataset2

--- source/dataloaders/project_2_dataset.py
+++ source/dataloaders/project_2_dataset.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
-from typing import Tuple, Optional, Union
+from typing import Tuple, Optional, Union, Set
 from torch.utils.data import Dataset
 
 import torch
 import torchaudio
 from torch import Tensor
+import librosa
 
 HASH_DIVIDER = "_nohash_"
 EXCEPT_FOLDER = "_background_noise_"
+
+
 
 class Project_2_Dataset(Dataset):
 
@@ -16,7 +19,8 @@ class Project_2_Dataset(Dataset):
             self,
             root: Union[str, Path],
             subset: Optional[str] = None,
-            transform: torch.nn.Sequential = None
+            transform: torch.nn.Sequential = None,
+            labels: Set = None
         ) -> None:
 
         assert subset is None or subset in ["training", "validation", "testing"], (
@@ -27,7 +31,12 @@ class Project_2_Dataset(Dataset):
         root = os.fspath(root)
         self._path = root
         self._transform = transform
-        
+
+        if labels == None:
+            self._labels = set(['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence'])
+        else:
+            self._labels = labels
+    
 
         if subset == "validation":
             self._walker = self.__load_list(self._path, "validation_list.txt")
@@ -46,7 +55,7 @@ class Project_2_Dataset(Dataset):
             self._walker = [w for w in walker if HASH_DIVIDER in w and EXCEPT_FOLDER not in w]
 
         
-    def __getitem__(self, n: int) -> Tuple[Tensor, int]:
+    def __getitem__(self, n: int) -> Tuple[Tensor, int, str, str, int]:
         """Load the n-th sample from the dataset.
 
         Args:
@@ -57,11 +66,16 @@ class Project_2_Dataset(Dataset):
             ``(waveform, sample_rate, label, speaker_id, utterance_number)``
         """
         fileid = self._walker[n]
-        return self.__load_speechcommands_item(fileid, self._path)
+
+        waveform, sample_rate, label, speaker_id, utterance_number = self.__load_speechcommands_item(fileid, self._path)
+        label = label if label in self._labels else 'unknown'
+        
+        return waveform, label
 
 
     def __len__(self) -> int:
         return len(self._walker)
+
 
     def __load_list(self, root, *filenames):
         output = []
@@ -71,9 +85,11 @@ class Project_2_Dataset(Dataset):
                 output += [os.path.normpath(os.path.join(os.path.join(root, 'audio'), line.strip())) for line in fileobj]
         return output
 
+
     def __load_speechcommands_item(self, filepath: str, path: str) -> Tuple[Tensor, int, str, str, int]:
         relpath = os.path.relpath(filepath, path)
         label, filename = os.path.split(relpath)
+        label = label.split('\\')[1]
        
         speaker, _ = os.path.splitext(filename)
         speaker, _ = os.path.splitext(speaker)
@@ -82,13 +98,25 @@ class Project_2_Dataset(Dataset):
         utterance_number = int(utterance_number)
 
         # Load audio
-        waveform, sample_rate = torchaudio.load(filepath)
-
+        waveform, sample_rate = self.__load_wav_file(filepath)
+        
         if self._transform is not None:
-             waveform, sample_rate = self.__transform(waveform, sample_rate)
+            waveform, sample_rate = self.__transform(waveform, sample_rate)
 
         return waveform, sample_rate, label, speaker_id, utterance_number
     
 
     def __transform(self, waveform: Tensor, sample_rate: int) -> Tuple[Tensor, int]:
-        pass
+        
+        waveform = self._transform(waveform)
+        return waveform, sample_rate
+
+
+    def __load_wav_file(self, filepath) -> Tuple[Tensor, int]:
+
+        waveform, sample_rate = librosa.load(filepath)
+        if len(waveform) != sample_rate:
+            waveform = librosa.util.fix_length(waveform, size=sample_rate, mode='edge')
+        waveform = torch.tensor(waveform)
+        waveform = torch.unsqueeze(waveform, 0)
+        return waveform, sample_rate
