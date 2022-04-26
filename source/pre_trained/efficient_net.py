8db61e622766dcfccda8301cf999004136c69699
Gosia Hadasz <Ghadasz@interia.eu>
2022-04-26 23:05:57 +0200

    efficient_net on spectogram

--- source/pre_trained/efficient_net.py
+++ source/pre_trained/efficient_net.py
@@ -7,14 +7,14 @@ from source.custom_cnn.image_classification_base import ImageClassificationBase
 
 
 class PretrainedEff_cnn(ImageClassificationBase):
-    def __init__(self) -> None:
+    def __init__(self,number_of_classes) -> None:
         super(PretrainedEff_cnn,self).__init__()
 
-        self.model =  models.efficientnet_b0(pretrained=True)
-        # for params in self.model.parameters():
-        #   params.requires_grad = False
-    
-        self.model.classifier[1]=nn.Linear(in_features=1280,out_features= 10)
+        self.model =  models.efficientnet_b0(pretrained=True) 
+        conv_weight = self.model.features[0][0].weight
+        self.model.features[0][0].in_channels = 1  
+        self.model.features[0][0].weight = torch.nn.Parameter(conv_weight.sum(dim = 1, keepdim=True))
+        self.model.classifier[1]=nn.Linear(in_features=1280,out_features= number_of_classes)
         
 
     def forward(self,x):
--- source/transformers.py
+++ source/transformers.py
@@ -6,53 +6,56 @@ import random
 import torch
 import torchaudio
 
+
 class TrainTrasformersFactory:
 
     @staticmethod
     def get_train_transformer():
         return torchvision.transforms.Compose([
-           
+
             torchvision.transforms.ToTensor(),
             # Standardize each channel of the image
             torchvision.transforms.Normalize([0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5])])
+                                             [0.5, 0.5, 0.5])])
 
-
-    @staticmethod 
+    @staticmethod
     def get_transform_with_cut_out_custom():
         return torchvision.transforms.Compose([
-           
+
             torchvision.transforms.ToTensor(),
-            CutOut(random.randint(1,4),random.randint(3,8)),
+            CutOut(random.randint(1, 4), random.randint(3, 8)),
             # Standardize each channel of the image
             torchvision.transforms.Normalize([0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5])])
-
+                                             [0.5, 0.5, 0.5])])
 
     @staticmethod
     def get_transformer_kaggle():
         """
         https://www.kaggle.com/vikasbhadoria/cifar10-high-accuracy-model-build-on-pytorch
         """
-        return transforms.Compose([transforms.Resize((244,244)),  #resises the image so it can be perfect for our model.
-                                      transforms.RandomHorizontalFlip(), # FLips the image w.r.t horizontal axis
-                                      transforms.RandomRotation(10),     #Rotates the image to a specified angel
-                                      transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)), #Performs actions like zooms, change shear angles.
-                                      transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # Set the color params
-                                      transforms.ToTensor(), # comvert the image to tensor so that it can work with torch
-                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #Normalize all the images
-                               ])
+        return transforms.Compose([transforms.Resize((244, 244)),  # resises the image so it can be perfect for our model.
+                                   transforms.RandomHorizontalFlip(),  # FLips the image w.r.t horizontal axis
+                                   # Rotates the image to a specified angel
+                                   transforms.RandomRotation(10),
+                                   # Performs actions like zooms, change shear angles.
+                                   transforms.RandomAffine(
+                                       0, shear=10, scale=(0.8, 1.2)),
+                                   # Set the color params
+                                   transforms.ColorJitter(
+                                       brightness=0.2, contrast=0.2, saturation=0.2),
+                                   transforms.ToTensor(),  # comvert the image to tensor so that it can work with torch
+                                   # Normalize all the images
+                                   transforms.Normalize(
+                                       (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                   ])
 
-
-   
     @staticmethod
     def get_transformer_cifar_auto():
         return transforms.Compose([
-            transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10), 
-			transforms.ToTensor(), 
+            transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
+            transforms.ToTensor(),
             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
         ])
-
 
     @staticmethod
     def get_transformer_only_rotation():
@@ -62,30 +65,40 @@ class TrainTrasformersFactory:
             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
         ])
 
-
     @staticmethod
     def get_transformer_kaggle_vgg():
-        return transforms.Compose([transforms.Resize((32,32)),  #resises the image so it can be perfect for our model.
-                                      transforms.RandomHorizontalFlip(), # FLips the image w.r.t horizontal axis
-                                      transforms.RandomRotation(10),     #Rotates the image to a specified angel
-                                      transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)), #Performs actions like zooms, change shear angles.
-                                      transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # Set the color params
-                                      transforms.ToTensor(), # comvert the image to tensor so that it can work with torch
-                                      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]) #Normalize all the images
-
+        return transforms.Compose([transforms.Resize((32, 32)),  # resises the image so it can be perfect for our model.
+                                   transforms.RandomHorizontalFlip(),  # FLips the image w.r.t horizontal axis
+                                   # Rotates the image to a specified angel
+                                   transforms.RandomRotation(10),
+                                   # Performs actions like zooms, change shear angles.
+                                   transforms.RandomAffine(
+                                       0, shear=10, scale=(0.8, 1.2)),
+                                   # Set the color params
+                                   transforms.ColorJitter(
+                                       brightness=0.2, contrast=0.2, saturation=0.2),
+                                   transforms.ToTensor(),  # comvert the image to tensor so that it can work with torch
+                                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])  # Normalize all the images
 
     @staticmethod
     def get_transformer_resample():
         return torch.nn.Sequential(torchaudio.transforms.Resample(new_freq=8_000))
 
+    @staticmethod
+    def get_transformer_spectogram_eff_new():
+        return transforms.Compose([torch.nn.Sequential(torchaudio.transforms.Resample(new_freq=8_000),
+                                   torchaudio.transforms.Spectrogram()),
+                                   transforms.Resize((224, 224)),
+                                   transforms.Normalize((0.5), (0.5))])
+
 
 class TestTransformersFactory:
     @staticmethod
-    def get_transformer_efficient():    
+    def get_transformer_efficient():
         return torchvision.transforms.Compose([
-          transforms.Resize((256, 224)),
-        torchvision.transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+            transforms.Resize((256, 224)),
+            torchvision.transforms.ToTensor(),
+            transforms.Normalize((0.5), (0.5))])
 
     @staticmethod
     def get_test_transformer():
@@ -93,36 +106,44 @@ class TestTransformersFactory:
             torchvision.transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-
-
     @staticmethod
     def get_transformer_kaggle():
         """
            validate on get_transformer_kaggle just normalization 
         """
         return torchvision.transforms.Compose([
-            transforms.Resize((244,244)), 
-        torchvision.transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-
+            transforms.Resize((244, 244)),
+            torchvision.transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     @staticmethod
     def get_transformer_kaggle_all():
         """
            validate on get_transformer_kaggle with trasformation
         """
-        return transforms.Compose([transforms.Resize((32,32)),  #resises the image so it can be perfect for our model.
-                                      transforms.RandomHorizontalFlip(), # FLips the image w.r.t horizontal axis
-                                      transforms.RandomRotation(10),     #Rotates the image to a specified angel
-                                      transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)), #Performs actions like zooms, change shear angles.
-                                      transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # Set the color params
-                                      transforms.ToTensor(), # comvert the image to tensor so that it can work with torch
-                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #Normalize all the images
-                               ])
-
+        return transforms.Compose([transforms.Resize((32, 32)),  # resises the image so it can be perfect for our model.
+                                   transforms.RandomHorizontalFlip(),  # FLips the image w.r.t horizontal axis
+                                   # Rotates the image to a specified angel
+                                   transforms.RandomRotation(10),
+                                   # Performs actions like zooms, change shear angles.
+                                   transforms.RandomAffine(
+                                       0, shear=10, scale=(0.8, 1.2)),
+                                   # Set the color params
+                                   transforms.ColorJitter(
+                                       brightness=0.2, contrast=0.2, saturation=0.2),
+                                   transforms.ToTensor(),  # comvert the image to tensor so that it can work with torch
+                                   # Normalize all the images
+                                   transforms.Normalize(
+                                       (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                   ])
 
     @staticmethod
     def get_transformer_resample():
         return torch.nn.Sequential(torchaudio.transforms.Resample(new_freq=8_000))
 
+    @staticmethod
+    def get_transformer_spectogram_eff_new():
+        return transforms.Compose([torch.nn.Sequential(torchaudio.transforms.Resample(new_freq=8_000),
+                                   torchaudio.transforms.Spectrogram()),
+                                   transforms.Resize((224, 224)),
+                                   transforms.Normalize((0.5), (0.5))])
