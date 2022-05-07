c7e9a367c3c6a0d06a6fe3cc03e9b30dbfe5af0a
Paweł Golik <01142171@pw.edu.pl>
2022-05-07 23:40:25 +0200

    main_pg fix

--- main_pg.py
+++ main_pg.py
@@ -4,9 +4,9 @@ from source.audiotransformers import AudioTrainTrasformersFactory, AudioTestTras
 from source.project2.training_scripts import project2_tune, PROJECT2MODE, train_one_vs_one
 
 
-model = VGGLikeParametrized(n_output=12)
+model = VGGLikeParametrized(n_output=12, p_last_droput=0.3)
 opt = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = torch.nn.CrossEntropyLoss()
 train_transform  = AudioTrainTrasformersFactory.get_train_transformer_spectogram_mel()
 test_transform = AudioTestTrasformersFactory.get_test_transformer_spectogram_mel()
-train_one_vs_one(12, model, opt, criterion, train_transform, test_transform, batch_size=32, n_epochs=100, device='cuda', trial_name='conv2d_vgglikeparametrized')
+train_one_vs_one(12, model, opt, criterion, train_transform, test_transform, batch_size=32, n_epochs=100, device='cuda', trial_name='conv2d_vgglikeparametrized_p03')
