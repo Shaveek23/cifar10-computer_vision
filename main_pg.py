a2352c3f719d23398cd89cbec4a04fcbb3c082f9
Paweł Golik <01142171@pw.edu.pl>
2022-05-07 20:13:37 +0200

    adding M5 12 classes

--- main_pg.py
+++ main_pg.py
@@ -1,59 +1,12 @@
 import torch
-from source.custom_cnn.project2.vgglike import VGGLike
-from source.custom_cnn.project2.VGGLikeParametrized import VGGLikeParametrized
+from source.custom_cnn.conv1d import M5
 from source.audiotransformers import AudioTrainTrasformersFactory, AudioTestTrasformersFactory
-from source.project2.training_scripts import project2_tune, PROJECT2MODE
-
-for n_classes in [31, 12, 10]:
-    config = {
-        'net': [
-            {
-                'type': VGGLike,
-                'arguments': {
-                'p_last_droput': [0.1, 0.2, 0.3],
-                'n_output': [n_classes] 
-                }
-            },
-            {
-                'type': VGGLikeParametrized,
-                'arguments': {
-                'p_last_droput': [0.1, 0.2, 0.3],
-                'n_output': [n_classes],
-                'K': [4, 8, 16, 32] 
-                }
-            }
-        ],
-        'optimizer': [
-            {
-                'type': torch.optim.Adam,
-                'arguments': {
-                    'lr': [0.01, 0.05, 0.001],
-                    'weight_decay': [0, 0.0001]
-                }
-            }
-        ],
-        'transform': [
-            {
-                'type': {
-                    'train': AudioTrainTrasformersFactory.get_train_transformer_spectogram_mel,
-                    'test': AudioTestTrasformersFactory.get_test_transformer_spectogram_mel
-                },
-                'arguments': {
-
-                }
-            }
-        ],
-        'batch_size': [32, 64, 128]
-    }
-
-    criterion = torch.nn.CrossEntropyLoss()
-    
-    if n_classes == 31:
-        project2_tune(config, criterion, device='cuda', n_trials=10, trial_name=f'conv2d_noaug_nclass_{n_classes}', n_epochs=50, mode=PROJECT2MODE.ONE_VS_ONE, n_classes=n_classes)
-    else:
-        project2_tune(config, criterion, device='cuda', n_trials=10, trial_name=f'conv2d_noaug_nclass_{n_classes}', n_epochs=30, mode=PROJECT2MODE.ONE_VS_ONE, n_classes=n_classes)
+from source.project2.training_scripts import project2_tune, PROJECT2MODE, train_one_vs_one
 
 
-
-
-
+model = M5(n_output=12, n_channel=32, stride=4)
+opt = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = torch.nn.CrossEntropyLoss()
+train_transform  = AudioTrainTrasformersFactory.get_train_tranformer_resampled(new_freq=16_000)
+test_transform = AudioTestTrasformersFactory.get_test_tranformer_resampled(new_freq=16_000)
+train_one_vs_one(12, model, opt, criterion, n_epochs=100, device='cuda', trial_name='conv1d_12_best')
