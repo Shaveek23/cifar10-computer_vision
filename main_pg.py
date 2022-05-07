de4bcab7bba8a6b3fded8472ff268c8bd9d48a1b
Paweł Golik <01142171@pw.edu.pl>
2022-05-07 21:33:28 +0200

    main_pg fix

--- main_pg.py
+++ main_pg.py
@@ -1,12 +1,12 @@
 import torch
-from source.custom_cnn.conv1d import M5
+from source.custom_cnn.project2.vgglike import VGGLike
 from source.audiotransformers import AudioTrainTrasformersFactory, AudioTestTrasformersFactory
 from source.project2.training_scripts import project2_tune, PROJECT2MODE, train_one_vs_one
 
 
-model = M5(n_output=12, n_channel=32, stride=4)
+model = VGGLike(n_output=12)
 opt = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = torch.nn.CrossEntropyLoss()
-train_transform  = AudioTrainTrasformersFactory.get_train_tranformer_resampled(new_freq=16_000)
-test_transform = AudioTestTrasformersFactory.get_test_tranformer_resampled(new_freq=16_000)
-train_one_vs_one(12, model, opt, criterion, train_transform, test_transform, batch_size=32, n_epochs=100, device='cuda', trial_name='conv1d_12_best')
+train_transform  = AudioTrainTrasformersFactory.get_train_transformer_spectogram_mel()
+test_transform = AudioTestTrasformersFactory.get_test_transformer_spectogram_mel()
+train_one_vs_one(12, model, opt, criterion, train_transform, test_transform, batch_size=32, n_epochs=100, device='cuda', trial_name='conv2d_vgglike')
